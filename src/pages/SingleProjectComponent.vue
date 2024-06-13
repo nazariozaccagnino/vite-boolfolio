9dc46ab67e4a555b65d58a9f54e368a51287fa5d
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-06-13 17:05:09 +0200

    fixing routes and cards

--- src/pages/SingleProjectComponent.vue
+++ src/pages/SingleProjectComponent.vue
@@ -1,10 +1,21 @@
 <template>
     <div v-if="project">
-        <!-- <img :src="getImage" class="postimg" :alt="project.title"> -->
-        <h1 class="my-4">{{ project.title }}</h1>
-        <p>{{ project.content }}</p>
-        <!-- <p>{{ project.category?.name }}</p> -->
-        
+        <div class="container position-relative">
+            <!-- <p>{{ project.category?.name }}</p> -->
+            <div class="card position-absolute top-0 start-50 translate-middle-x">
+                <h4 class="card-header text-center">
+                    {{ project.title }}
+                </h4>
+                <img :src="getImage" class="card-img-top" :alt="project.title">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">{{ project.content }}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </template>
 
@@ -22,23 +33,30 @@ export default {
     },
     methods: {
         getProject() {
-             console.log(this.$route);
-            axios.get(this.store.apiBaseUrl + '/projects/'+this.$route.params.slug).then((res) => {
+            console.log(this.$route);
+            axios.get(this.store.apiBaseUrl + '/projects/' + this.$route.params.slug).then((res) => {
                 console.log(res.data.results);
                 this.project = res.data.results;
+                console.log(this.project);
             })
         }
     },
     computed: {
         getImage() {
-            return this.item.image? this.store.imgBasePath + this.item.image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png';
+            return this.project.image ? this.store.imgBasePath + this.project.image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png';
         }
+
     },
     mounted() {
         this.getProject();
+
     },
 
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card{
+    width: 50rem;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -21,6 +21,7 @@ const router = createRouter({
             name: 'single-project',
             component: SingleProjectComponent,
         },
+        
     ]
 });
 export { router };
